125c39f84137babfba59e3ec45ee289da9cfa581
Zsolt Sándor <ezsolt@gmail.com>
2015-02-10 17:09:51 +0100

    Merge branch 'master' of https://github.com/graphis/flight-app
    Conflicts:
    	root/index.php

--- app/views/404.php
+++ app/views/404.php
@@ -1,6 +1,25 @@
-404
+
+
 <?php
 
+
+
+function parseRequestHeaders() {
+    $headers = array();
+    foreach($_SERVER as $key => $value) {
+        if (substr($key, 0, 5) <> 'HTTP_') {
+            continue;
+        }
+        $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+        $headers[$header] = $value;
+    }
+    return $headers;
+}
+
+// print_r( parseRequestHeaders() );
+
+//
+var_dump(http_response_code());
 
 
 // echo $name;
@@ -10,5 +29,22 @@ if (isset($array)) {
 }
 
 
+$messages = array('Aw, crap!', 'Bloody Hell!', 'Uh Oh!', 'Nope, not here...', 'Huh?');
+
+echo $messages[array_rand($messages)];
+echo "\nThe page you are looking could not be found.";
+echo 'Sorry';
 
 
+
+define('FLIGHT_END_TIME', microtime(TRUE));
+
+
+// print_r(FLIGHT_START_TIME - FLIGHT_END_TIME);
+
+
+
+?>
+///
+ddd
+...
